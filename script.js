e7468e39a200b7333ec53345f2c04b1fa09a7a9d
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-09 20:58:00 -0300

    add Ecolha uma cor

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const rgbColor = document.getElementById('rgb-color');
+const answer = document.getElementById('answer');
+const colorsBlock = document.getElementById('colors');
+const resetGame = document.getElementById('reset-game');
+
 function randomNumber(n) {
   return Math.floor(Math.random() * n);
 }
@@ -11,16 +16,18 @@ function randomColor() {
 
 function startGame() {
   const colors = document.querySelectorAll('.ball');
-  for (let color of colors) {
-    color.style.backgroundColor = randomColor();
+  answer.innerHTML = 'Escolha uma cor';
+  // for (let color of colors) {
+  //   color.style.backgroundColor = randomColor();
+  // }
+  for (let i = 0; i < colors.length; i += 1) {
+    colors[i].style.backgroundColor = randomColor();
   }
+  
   rgbColor.innerHTML = colors[randomNumber(colors.length)].style.backgroundColor;
 }
 
-const rgbColor = document.getElementById('rgb-color');
-const answer = document.getElementById('answer');
-const colorsBlock = document.getElementById('colors');
-const resetGame = document.getElementById('reset-game');
+resetGame.addEventListener('click', startGame)
 
 window.onload = () => {
   startGame();
@@ -34,8 +41,4 @@ window.onload = () => {
       }
     }
   });
-  resetGame.addEventListener('click', () => {
-    answer.innerHTML = '';
-    startGame();
-  });
-}
+};
