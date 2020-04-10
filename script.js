78c9539fec44c99b9b94d564bf9820dc063152ee
orlandomessias <programadorlando@gmail.com>
2020-04-09 22:18:48 -0300

    show the answer if correct or wrong

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let rightColor = '';
 const rgbValue = document.getElementById('rgb-color');
 const balls = document.querySelectorAll('.ball');
+const answer = document.getElementById('answer');
+
+answer.innerHTML = 'Escolha uma cor';
 
 for (let i = 0; i < 3; i += 1) {
   let randomic = Math.floor(Math.random() * 256);
@@ -23,3 +26,14 @@ for (let i = 0; i < 6; i += 1) {
 
 const correctBall = Math.floor(Math.random() * 6);
 balls[correctBall].style.backgroundColor = `rgb(${rightColor})`;
+
+for (let i = 0; i < balls.length; i += 1) {
+  balls[i].addEventListener('click', () => {
+    if (balls[i].style.backgroundColor === balls[correctBall].style.backgroundColor) {
+      answer.innerHTML = 'Acertou!';
+    }
+    else {
+      answer.innerHTML = 'Errou! Tente novamente!';
+    }
+  });
+}
