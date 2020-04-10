f581e745ed128e1ef4a9ae7484b0b0cdd103a5c9
orlandomessias <programadorlando@gmail.com>
2020-04-10 11:39:34 -0300

    restart game button functionality

--- script.js
+++ script.js
@@ -3,38 +3,51 @@ let rightColor = '';
 const rgbValue = document.getElementById('rgb-color');
 const balls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
+let imgCongrats = document.getElementById('img-congrats');
+let correctBall = 0;
 
-answer.innerHTML = 'Escolha uma cor';
+function resetGame(){
+  answer.innerHTML = 'Escolha uma cor';
 
-for (let i = 0; i < 3; i += 1) {
-  let randomic = Math.floor(Math.random() * 256);
-  rightColor += `${randomic},`;
-}
-rightColor = rightColor.substr(0, rightColor.length - 1);
-rgbValue.innerHTML = `(${rightColor})`;
-
-let color = '';
-for (let i = 0; i < 6; i += 1) {
-  for (let j = 0; j < 3; j += 1) {
+  for (let i = 0; i < 3; i += 1) {
     let randomic = Math.floor(Math.random() * 256);
-    color += `${randomic},`;
+    rightColor += `${randomic},`;
   }
-  color = color.substr(0, color.length - 1);
-  balls[i].style.backgroundColor = `rgb(${color})`;
-  color = '';
+  rightColor = rightColor.substr(0, rightColor.length - 1);
+  rgbValue.innerHTML = `(${rightColor})`;
+
+  let color = '';
+  for (let i = 0; i < 6; i += 1) {
+    for (let j = 0; j < 3; j += 1) {
+      let randomic = Math.floor(Math.random() * 256);
+      color += `${randomic},`;
+    }
+    color = color.substr(0, color.length - 1);
+    balls[i].style.backgroundColor = `rgb(${color})`;
+    color = '';
+  }
+
+  correctBall = Math.floor(Math.random() * 6);
+  balls[correctBall].style.backgroundColor = `rgb(${rightColor})`;
+
 }
 
-const correctBall = Math.floor(Math.random() * 6);
-balls[correctBall].style.backgroundColor = `rgb(${rightColor})`;
+resetGame();
 
 for (let i = 0; i < balls.length; i += 1) {
   balls[i].addEventListener('click', () => {
     if (balls[i].style.backgroundColor === balls[correctBall].style.backgroundColor) {
       answer.innerHTML = 'Acertou!';
-      document.getElementById('img-congrats').src = 'imgs/congrats.png';
+      imgCongrats.src = 'imgs/congrats.png';
     }
     else {
       answer.innerHTML = 'Errou! Tente novamente!';
     }
   });
 }
+
+document.getElementById('reset-game').addEventListener('click', () => {
+  imgCongrats.src = '';
+  rightColor = '';
+  resetGame();
+});
