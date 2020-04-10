8bc58a66d3e9c8d4ff80ec108953bc3b75324201
joaosoutto <joaosoutto92@gmail.com>
2020-04-10 19:54:54 -0300

    estilizando e corrigindo codeClimate

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function inicio() {
   const answer = document.getElementById('answer');
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].style.backgroundColor = randomColor();
+    answer.style.color = 'black';
     answer.innerHTML = 'Escolha uma cor';
   }
   rgbColor.innerHTML = colors[randomRGB(colors.length)].style.backgroundColor;
@@ -27,19 +28,22 @@ const resetButton = document.getElementById('reset-game');
 resetButton.addEventListener('click', inicio);
 
 //  clicar na bola:
-balls.addEventListener('click', (event) => {
+let points = null;
 const balls = document.getElementById('balls');
-const score = document.getElementById('score');
-const rgbColor = document.getElementById('rgb-color');
+balls.addEventListener('click', (event) => {
+  const score = document.getElementById('score');
+  const rgbColor = document.getElementById('rgb-color');
+  const answer = document.getElementById('answer');
 
   if (event.target !== balls) {
     if (event.target.style.backgroundColor === rgbColor.innerHTML) {
+      answer.style.color = 'green';
       answer.innerHTML = 'Acertou!';
       points += 3;
       score.innerHTML = `Pontos: ${points}`;
     } else {
+      answer.style.color = 'rgb(153,24,24)';
       answer.innerHTML = 'Errou! Tente novamente!';
     }
   }
 });
-let points = null;
