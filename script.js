ff1875cba09d5972fef12c418468ee877d38b6a7
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-12 15:04:18 -0300

    começando o js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Gerando cor aleatória
+function randomColor (n) {
+  return Math.floor(Math.random() * n);
+}
+
+function randomBoard () {
+  for (let i = 0; i < 6; i += 1){
+    
+  }
+}
+
+// Lendo entrada do Botão
+let buttom = document.getElementById('reset');
+buttom.addEventListener('click', function () {
+
+})
