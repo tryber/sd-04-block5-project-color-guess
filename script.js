90d0438c43c02327bc6788fee84c252117274dfa
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-13 17:59:43 -0300

    created balls and added colors in random

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function rgbRandom() {
 }
 
 rgbColor.innerHTML = rgbRandom();
+const balls = document.querySelectorAll('.ball');
+const colorIndex = Math.floor(Math.random() * 5);
+for (let i = 0; i < balls.length; i += 1) {
+  if (i === colorIndex){
+    balls[i].style.backgroundColor = `rgb${rgbColor.innerHTML}`;
+  }  else {
+    balls[i].style.backgroundColor = `rgb${rgbRandom()}`;
+  }
+}
