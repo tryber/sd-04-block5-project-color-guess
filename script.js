bf3b7f3439a015de912e830382f433c20de303e6
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-13 17:55:05 -0300

    corrigindo codeclimate

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Leituras
-let ball = document.getElementsByClassName('ball');
+const ball = document.getElementsByClassName('ball');
 const colorGuess = document.getElementById('rgb-color');
-let resultado = document.getElementById('answer');
+const resultado = document.getElementById('answer');
 
 // Gerando cor aleatória
-function randomNumber (n) {
+function randomNumber(n) {
   return Math.floor(Math.random() * n);
 }
 
-function randomBoard () {
-    ball[0].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
-    ball[1].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
-    ball[2].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
-    ball[3].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
-    ball[4].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
-    ball[5].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
-  }
+function randomBoard() {
+  ball[0].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+  ball[1].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+  ball[2].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+  ball[3].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+  ball[4].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+  ball[5].style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(256)}, ${randomNumber(256)})`;
+}
 
-function inicial () {
+function inicial() {
   randomBoard();
   colorGuess.innerHTML = ball[randomNumber(5)].style.backgroundColor;
   resultado.innerHTML = 'Escolha uma cor!';
@@ -25,23 +25,23 @@ function inicial () {
 
 window.onload = function () {
   inicial();
-}
-
-// Lendo entrada do Botão
-let buttom = document.getElementById('reset');
-buttom.addEventListener('click', inicial);
-
-// Add event listener para as cores
-for (let i = 0; i < 6; i += 1){
-  ball[i].addEventListener('click', conferencia)
-}
+};
 
 function conferencia() {
   resultado.innerHTML = '';
   const cor = event.target.style.backgroundColor;
-  if (cor === colorGuess.innerText){
-    resultado.innerHTML = "Acertou!"
+  if (cor === colorGuess.innerText) {
+    resultado.innerHTML = 'Acertou!';
   } else {
-    resultado.innerHTML = "Errou! Tente novamente!"
+    resultado.innerHTML = 'Errou! Tente novamente!';
   }
+}
+
+// Lendo entrada do Botão
+const buttom = document.getElementById('reset');
+buttom.addEventListener('click', inicial);
+
+// Add event listener para as cores
+for (let i = 0; i < 6; i += 1) {
+  ball[i].addEventListener('click', conferencia);
 }
