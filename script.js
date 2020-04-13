6b54ce8cefa6f88b25308df2ceacdaf7a748a0b5
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-13 16:14:01 -0300

    added function random color to the sectior rgb-color

---
+++ script.js
@@ -0,0 +1,9 @@
+const rgbColor = document.getElementById('rgb-color');
+function rgbRandom() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `(${r}, ${g}, ${b})`;
+}
+
+rgbColor.innerHTML = rgbRandom();
