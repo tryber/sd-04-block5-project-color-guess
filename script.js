1bbd31dbf0e929e01a23ad1db79b95bd6a2a37eb
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-14 19:23:32 -0300

    fix wrong text change when click on button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 
 }// end of window.onload
+const balls = document.querySelectorAll('.ball');
 // return random number between 1 and 255
 function aleatoire (){
 	return Math.round(Math.random()*255);
@@ -14,12 +15,11 @@ const resetGameElement = document.querySelector('#reset-game');
 resetGameElement.addEventListener('click',button3changestyle);
 function button3changestyle (){
 	ballsGetRandomColor();
-	document.querySelector('#title').innerHTML = 'Adivinhe a cor!';
+	document.querySelector('#answer').innerHTML = 'Escolha uma cor';
 };
-
 // function all ball get a random color and set one of the color to h2 in a text form 
+
 function ballsGetRandomColor (){
-const balls = document.querySelectorAll('.ball');
 const rgbColorElement = document.querySelector('#rgb-color');
 let randomBall = Math.round(Math.random()*5) + 1;
 console.log('randomBall',randomBall);
@@ -30,4 +30,15 @@ for (i = 0; i < balls.length; i += 1){
 }
 }
 ballsGetRandomColor()
+//function click on ball print a message 
+
+
+// balls.addEventListener('click',button3changestyle);
+// console.log('out');
+// for (i = 1; i < 7 ; i += 1){
+// function button3changestyle (){
+// console.log('inside!');
+// };
+// };
+
 
