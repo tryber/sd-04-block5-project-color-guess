a5f06d158842dac9012c031426ca2c8904bd82ec
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-14 16:30:50 -0300

    Generate random and correct color answers on page load in random position

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const colorToGuess = getRandomColor();
 document.querySelector('#rgb-color').innerHTML = `(${colorToGuess.red}, ${colorToGuess.green}, ${colorToGuess.blue})`;
 
+const colorAnsContainer = document.querySelector('#color-answers');
+const colorAnswers = generateAnswers(colorToGuess, 5);
+for (const color in colorAnswers) {
+  if (colorAnswers.hasOwnProperty(color)) {
+    const colorData = colorAnswers[color];
+    const colorAnsElement = document.createElement('div');
+    colorAnsElement.className = 'ball';
+    colorAnsElement.style.backgroundColor = `rgb(${colorData.red}, ${colorData.green}, ${colorData.blue})`;
+
+    colorAnsContainer.appendChild(colorAnsElement);
+  }
+}
+
 function randomNumber(maxSize) {
   const number = Math.round(Math.random() * maxSize);
 
