db5a0fcdfb4b336496da53879353a7c94a839c24
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-13 23:28:16 -0300

    added button to reset the game

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ function rgbRandom() {
 
 rgbColor.innerHTML = rgbRandom();
 const balls = document.querySelectorAll('.ball');
-const colorIndex = Math.floor(Math.random() * 5);
-for (let i = 0; i < balls.length; i += 1) {
-  if (i === colorIndex) {
-    balls[i].style.backgroundColor = `rgb${rgbColor.innerHTML}`;
-  } else {
-    balls[i].style.backgroundColor = `rgb${rgbRandom()}`;
+
+function insertRandomColors() {
+  const colorIndex = Math.floor(Math.random() * 5);
+  for (let i = 0; i < balls.length; i += 1) {
+    if (i === colorIndex) {
+      balls[i].style.backgroundColor = `rgb${rgbColor.innerHTML}`;
+    } else {
+      balls[i].style.backgroundColor = `rgb${rgbRandom()}`;
+    }
   }
 }
+
+insertRandomColors();
 
 function verifyColor(element) {
   const guessedColor = element.style.backgroundColor;
@@ -29,4 +34,10 @@ const selectColor = document.getElementById('select-color');
 selectColor.addEventListener('click', function () {
   if (verifyColor(event.target)) p.innerHTML = 'Acertou!';
   else p.innerHTML = 'Errou! Tente novamente!';
+});
+const buttonReset = document.getElementById('reset-game');
+buttonReset.addEventListener('click', function () {
+  rgbColor.innerHTML = rgbRandom();
+  insertRandomColors();
+  p.innerHTML = 'Escolha uma cor';
 });
