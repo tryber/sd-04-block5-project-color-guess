d181e2ee6c97697b8ad883af36513af79992910e
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-14 19:02:48 -0300

    add fucntion all balls get random color and button reset

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+
+}// end of window.onload
+// return random number between 1 and 255
+function aleatoire (){
+	return Math.round(Math.random()*255);
+}
+//return a random color format rgb
+function randomcolor (){
+	return '('+ aleatoire() + ',' + aleatoire() + ','+ aleatoire() +')';
+}
+//function on click RESET button, active ballsGetRandomColor()
+const resetGameElement = document.querySelector('#reset-game');
+resetGameElement.addEventListener('click',ballsGetRandomColor);
+//function button3changestyle (){
+	//ballsGetRandomColor();
+	//console.log('Hello');
+//};
+
+// function all ball get a random color and set one of the color to h2 in a text form 
+function ballsGetRandomColor (){
+const balls = document.querySelectorAll('.ball');
+const rgbColorElement = document.querySelector('#rgb-color');
+let randomBall = Math.round(Math.random()*5) + 1;
+console.log('randomBall',randomBall);
+for (i = 0; i < balls.length; i += 1){
+	let tempColor = randomcolor();
+	balls[i].style.backgroundColor = 'rgb' + tempColor;
+	if (randomBall == i) rgbColorElement.innerHTML = tempColor;
+}
+}
+ballsGetRandomColor()
+
