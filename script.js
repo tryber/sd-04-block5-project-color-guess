27cf98287aa58853d0f3ee90d4b7da05fabe1925
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-15 18:10:57 -0300

    codeclimate fix

--- script.js
+++ script.js
@@ -1,58 +1,52 @@
-window.onload = function(){
-
-}// end of window.onload
+window.onload = function () {
+};//  end of window.onload
 let scorevalue = 0;
 const scoreElement = document.getElementById('score');
 scoreElement.innerHTML = 'Seu score é: ' + scorevalue;
 const balls = document.querySelectorAll('.ball');
 const rgbColorElement = document.querySelector('#rgb-color');
 // return random number between 1 and 255
-function aleatoire (){
-	return Math.round(Math.random()*255);
+function aleatoire() {
+  return Math.round(Math.random() * 255);
 }
-//return a random color format rgb
-function randomcolor (){
-	return '('+ aleatoire() + ', ' + aleatoire() + ', '+ aleatoire() +')';
+// return a random color format rgb
+function randomcolor() {
+  return '(' + aleatoire() + ', ' + aleatoire() + ', ' + aleatoire() + ')';
 }
-//function on click RESET button, active ballsGetRandomColor()
+//  function on click RESET button, active ballsGetRandomColor()
 const resetGameElement = document.querySelector('#reset-game');
-resetGameElement.addEventListener('click',button3changestyle);
-function button3changestyle (){
-	ballsGetRandomColor();
-//	console.log('Hello click');
-	document.querySelector('#answer').innerHTML = 'Escolha uma cor';
+resetGameElement.addEventListener('click', button3changestyle);
+function button3changestyle () {
+  ballsGetRandomColor();
+//  console.log('Hello click');
+  document.querySelector('#answer').innerHTML = 'Escolha uma cor';
 };
-// function all ball get a random color and set one of the color to h2 in a text form 
-function ballsGetRandomColor (){
-let randomBall = Math.round(Math.random()*5);
-//console.log('randomBall',randomBall);
-for (i = 0; i < balls.length; i += 1){
-	let tempColor = randomcolor();
-	balls[i].style.backgroundColor = 'rgb' + tempColor;
-	//console.log('i',i,balls[i].style.backgroundColor);
-	if (randomBall == i) {
-		//console.log('igual !!',balls[i].style.backgroundColor, tempColor);
-		rgbColorElement.innerHTML = tempColor;
-	}
+//  function all ball get a random color and set one of the color to h2 in a text form
+function ballsGetRandomColor() {
+  const randomBall = Math.round(Math.random() * 5);
+//  console.log('randomBall',randomBall);
+  for (let i = 0; i < balls.length; i += 1) {
+    cons tempColor = randomcolor();
+    balls[i].style.backgroundColor = 'rgb' + tempColor;
+    //  console.log('i',i,balls[i].style.backgroundColor);
+    if (randomBall === i) {
+      //console.log('igual !!',balls[i].style.backgroundColor, tempColor);
+      rgbColorElement.innerHTML = tempColor;
+    }
 }
 }
-ballsGetRandomColor()
-//function click on ball print a message 
+ballsGetRandomColor();
+//  function click on ball print a message
 document.body.addEventListener('click', function (event) {
-	const classname = event.target.className;
-	const targetbackground = event.target.style.backgroundColor;
-	testComparison = targetbackground == 'rgb' + rgbColorElement.innerHTML;
-	if (targetbackground == 'rgb' + rgbColorElement.innerHTML && classname.includes('ball')) {
-		document.querySelector('#answer').innerHTML = 'Acertou!';
-		scorevalue += 3	;
-		scoreElement.innerHTML = 'Seu score é: ' + scorevalue;;
-	}
-	else if (targetbackground !== 'rgb' + rgbColorElement.innerHTML && classname.includes('ball')) {
-		document.querySelector('#answer').innerHTML = 'Errou! Tente novamente!';
-		//console.log('loop errou');
-	}
-})
-
-
-
-
+  const classname = event.target.className;
+  const targetbackground = event.target.style.backgroundColor;
+  //testComparison = targetbackground === 'rgb' + rgbColorElement.innerHTML;
+  if (targetbackground === 'rgb' + rgbColorElement.innerHTML && classname.includes('ball')) {
+    document.querySelector('#answer').innerHTML = 'Acertou!';
+    scorevalue += 3;
+    scoreElement.innerHTML = 'Seu score é: ' + scorevalue;  } 
+    else if (targetbackground !== 'rgb' + rgbColorElement.innerHTML && classname.includes('ball')) {
+    document.querySelector('#answer').innerHTML = 'Errou! Tente novamente!';
+    //  console.log('loop errou');
+  }
+});
