a414024cc8ee1bf0656c755c96a802a59de1427a
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-15 09:33:41 -0300

    add function change text

--- script.js
+++ script.js
@@ -2,43 +2,52 @@ window.onload = function(){
 
 }// end of window.onload
 const balls = document.querySelectorAll('.ball');
+const rgbColorElement = document.querySelector('#rgb-color');
 // return random number between 1 and 255
 function aleatoire (){
 	return Math.round(Math.random()*255);
 }
 //return a random color format rgb
 function randomcolor (){
-	return '('+ aleatoire() + ',' + aleatoire() + ','+ aleatoire() +')';
+	return '('+ aleatoire() + ', ' + aleatoire() + ', '+ aleatoire() +')';
 }
 //function on click RESET button, active ballsGetRandomColor()
 const resetGameElement = document.querySelector('#reset-game');
 resetGameElement.addEventListener('click',button3changestyle);
 function button3changestyle (){
 	ballsGetRandomColor();
+	console.log('Hello click');
 	document.querySelector('#answer').innerHTML = 'Escolha uma cor';
 };
 // function all ball get a random color and set one of the color to h2 in a text form 
-
 function ballsGetRandomColor (){
-const rgbColorElement = document.querySelector('#rgb-color');
-let randomBall = Math.round(Math.random()*5) + 1;
-console.log('randomBall',randomBall);
+let randomBall = Math.round(Math.random()*5);
+//console.log('randomBall',randomBall);
 for (i = 0; i < balls.length; i += 1){
 	let tempColor = randomcolor();
 	balls[i].style.backgroundColor = 'rgb' + tempColor;
-	if (randomBall == i) rgbColorElement.innerHTML = tempColor;
+	//console.log('i',i,balls[i].style.backgroundColor);
+	if (randomBall == i) {
+		//console.log('igual !!',balls[i].style.backgroundColor, tempColor);
+		rgbColorElement.innerHTML = tempColor;
+	}
 }
 }
 ballsGetRandomColor()
 //function click on ball print a message 
+document.body.addEventListener('click', function (event) {
+	const classname = event.target.className;
+	const targetbackground = event.target.style.backgroundColor;
+	testComparison = targetbackground == 'rgb' + rgbColorElement.innerHTML;
+	if (targetbackground == 'rgb' + rgbColorElement.innerHTML && classname.includes('ball')) {
+		document.querySelector('#answer').innerHTML = 'Acertou!';
+	}
+	else if (targetbackground !== 'rgb' + rgbColorElement.innerHTML && classname.includes('ball')) {
+		document.querySelector('#answer').innerHTML = 'Errou! Tente novamente!';
+		//console.log('loop errou');
+	}
+})
 
 
-// balls.addEventListener('click',button3changestyle);
-// console.log('out');
-// for (i = 1; i < 7 ; i += 1){
-// function button3changestyle (){
-// console.log('inside!');
-// };
-// };
 
 
