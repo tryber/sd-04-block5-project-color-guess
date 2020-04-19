8b1bbb19d6427bc9a874483f6d2b96fbcf5f4b29
Julio Cesar <stininmatic@hotmail.com>
2020-04-19 01:25:11 -0300

    Jogo já funcionando

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-    
-    var rgbColor = document.querySelector("#rgb-color");
-    var balls = document.querySelectorAll(".ball");
-  
-    function generateColor(){
-    var r = Math.floor(Math.random()*256);
-    var g = Math.floor(Math.random()*256);
-    var b = Math.floor(Math.random()*256);
+var rgbColor = document.querySelector("#rgb-color");
+var balls = document.querySelectorAll(".ball");
+var answer = document.querySelector("#answer");
 
-    const rightColor = "(" + r + "," + g + "," + b + ")";
-      
-    return rightColor;
+function generateColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
 
+  const rightColor = "(" + r + "," + g + "," + b + ")";
+
+  return rightColor;
+}
+
+//   function mainColor(){
+for (let i = 0; i < balls.length; i++) {
+  balls[i].style.backgroundColor = "rgb" + generateColor();
+
+  //   }
+}
+
+var corEscolhida = balls[Math.floor(Math.random() * balls.length)];
+console.log(corEscolhida);
+rgbColor.innerHTML = corEscolhida.style.backgroundColor;
+
+for (let i = 0; i < balls.length; i++) {
+  balls[i].addEventListener("click", function () {
+    console.log("Tudo dando certo");
+    console.log(balls[i].style.backgroundColor);
+    console.log(corEscolhida.style.backgroundColor);
+    if (balls[i].style.backgroundColor === corEscolhida.style.backgroundColor) {
+      console.log(true);
+      answer.innerHTML = "Acertou!"
+    } else {
+      console.log(false);
+      answer.innerHTML = "Errou!"
     }
-
-
-
-
- //   function mainColor(){
-    for(let i=0 ; i<balls.length ; i++){
-        balls[i].style.backgroundColor = "rgb" + generateColor();     
-        
- //   }
-
-    }
-
-    var corEscolhida = balls[Math.floor(Math.random()*balls.length)];
-    console.log(corEscolhida);
-    rgbColor.innerHTML = corEscolhida.style.backgroundColor;
+  });
+}
