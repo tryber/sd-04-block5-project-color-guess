d189a839ff752300b9a72e645139ac6996156eae
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-19 10:03:41 -0300

    step6

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const paragraph = document.getElementById('rgb-color');
 const divs = document.querySelectorAll('.ball');
-const divMae = document.getElementById('flexbox');
-console.log(divs);
+const divMae = document.querySelector('.flexbox');
+const resposta = document.getElementById('answer');
+const bckpResposta = resposta.textContent;
+const botaoReiniciar = document.getElementById('reset-game');
 
 
 function randomColor() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
-  return rColor = `(${r}, ${b}, ${g})`
+  return rColor = `(${r}, ${b}, ${g})`;
 }
 let arrayBolas = [0, 1, 2, 3, 4, 5];
 function bolaPremiada() {
+  arrayBolas = [0, 1, 2, 3, 4, 5];
+  paragraph.innerHTML = rColor;
   let index = Math.floor(Math.random() * 6);
   divs[index].style.backgroundColor = `rgb${rColor}`;
   divs[index].classList.add('winner');
   arrayBolas.splice(index, 1);
 
-  return arrayBolas
+  return arrayBolas;
 }
 
 function colorirBolas() {
@@ -29,27 +33,32 @@ function colorirBolas() {
   }
 }
 
-// function clique(event) {
-//   const resposta = document.getElementById('answer');
-//   if (event.target.classList == 'winner') {
-//     resposta.innerHTML = 'Acertou!';
-//   }else {
-//     resposta.innerHTML = 'Errou! Tente novamente!';
-//   }
-// }
+function removeWinner() {
+  for (let i = 0; i < divs.length; i += 1) {
+    if (divs[i].classList.contains('winner')) {
+      divs[i].classList.remove('winner');
+    }
+  }
+}
+console.log(divs);
 
 window.onload = function () {
   randomColor();
-  paragraph.innerHTML = rColor;
-  this.bolaPremiada();
-  this.colorirBolas();
-  document.body.addEventListener('click', function(event) {
-    const resposta = document.getElementById('answer');
-    if (event.target.classList.contains('winner')) {
+  bolaPremiada();
+  colorirBolas();
+  divMae.addEventListener('click', function (event) {
+    if (event.target.classList.contains('ball') && event.target.classList.contains('winner')) {
       resposta.innerHTML = 'Acertou!';
-    }else {
+    } else {
       resposta.innerHTML = 'Errou! Tente novamente!';
     }
+  })
+  botaoReiniciar.addEventListener('click', function () {
+    removeWinner();
+    randomColor();
+    bolaPremiada();
+    colorirBolas();
+    resposta.textContent = bckpResposta;
   })
 }
 
