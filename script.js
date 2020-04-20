d3c21dc872eb4734843fcd1b2097704fea68b978
deboracosilveira <deboracosilveira@gmail.com>
2020-04-19 23:26:18 -0300

    requirement-4

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 // Variables
 const title = document.getElementById('title');
 const rgbColor = document.getElementById('rgb-color');
-const balls = document.querySelectorAll('.ball')
+const ballsContainer = document.getElementById('ballsContainer');
+const balls = document.querySelectorAll('.ball');
+const answer = document.getElementById('answer');
 
+// Generating a random number multiple of 255 
+function random() {
+  const random = Math.round(Math.random() * 255);
+  return random;
+}
 // Generating random rgb
 function generateRgb() {
-  function random() {
-    const random = Math.round(Math.random() * 255);
-    return random;
-  }
   const rgb = '(' + random() + ', ' + random() + ', ' + random() + ')';
   rgbColor.innerText = rgb;
 }
-//  
-generateRgb();
+// Comparing user's answer and the correct one
+for (let i = 0; i < balls.length; i += 1) {
+  balls[i].style.cursor = 'pointer';
+  balls[i].addEventListener('click', function (e) {
+    let correctAnswer = rgbColor.innerText;
+    correctAnswer = 'rgb' + correctAnswer;
+    const userAnswer = e.target.style.backgroundColor;
 
-// for (let i = 0; i < balls.length; i += 1) {
-//   console.log(balls[i]);
-//   balls[i].style.background = 
-// }
+    if (userAnswer === correctAnswer) {
+      answer.innerText = 'Acertou!';
+      console.log("correctAnswer");
+    } else {
+      answer.innerText = 'Errou! Tente novamente';
+      console.log("wrongAnswer");
+    }
+  });
+}
+// Generating balls background color
+function ballsBgColor() {
+  let randomNumber = Math.floor(Math.random() * balls.length)// Random number between 0 and 5
+  for (let i = 0; i < balls.length; i += 1) {
+    if (i === randomNumber) {// To put the answer in a random ball
+      let correctAnswer = rgbColor.innerText;
+      correctAnswer = 'rgb' + correctAnswer;
+      balls[i].style.backgroundColor = correctAnswer;
+      console.log(randomNumber)
+    } else {
+      const rgb = 'rgb(' + random() + ', ' + random() + ', ' + random() + ')';
+      balls[i].style.backgroundColor = rgb;
+    }
+  }
+}
+
+window.onload = function () {
+  generateRgb();
+  ballsBgColor();
+}
