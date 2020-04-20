c1d5a7ac36a8788f1cef28cbdfd99769e695fb18
igorgbr <igorgphotoarte@gmail.com>
2020-04-20 19:22:40 -0300

    codeclimate

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const colorFive = document.querySelector('#color-five');
 const colorSix = document.querySelector('#color-six');
 
 const textoRGB = document.querySelector('#rgb-color');
-const colorPainel = document.querySelector('#colors');
 const resposta = document.querySelector('#answer');
 const ball = document.querySelectorAll('.ball');
 const btnReinicia = document.querySelector('#reset-game');
@@ -23,89 +22,86 @@ function sortColor() {
   colorFive.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
   colorSix.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
 }
- 
+
 function apagaClass() {
-    colorOne.className = 'ball';
-    colorTwo.className = 'ball';
-    colorThree.className = 'ball';
-    colorFour.className = 'ball';
-    colorFive.className = 'ball';
-    colorSix.className = 'ball';
-  }
+  colorOne.className = 'ball';
+  colorTwo.className = 'ball';
+  colorThree.className = 'ball';
+  colorFour.className = 'ball';
+  colorFive.className = 'ball';
+  colorSix.className = 'ball';
+}
 
-  function colorCorrect(){
-    for(i = 0; i < ball.length; i += 1){
-    let texto = ((ball[(Number.parseInt(Math.random() * (6 - 1) + 1))].style.backgroundColor).toString()).replace('rgb', '');
-      textoRGB.innerHTML = texto;
-      
-    }
-
+function colorCorrect() {
+  for (let i = 0; i < ball.length; i += 1) {
+    const texto = ((ball[(parseInt(Math.random() * 6, 10))].style.backgroundColor).toString()).replace('rgb', '');
+    textoRGB.innerHTML = texto;
   }
-  function testaValor(a, b){
-    if(a != b) {
-      resposta.innerHTML = 'Errou! Tente novamente';
+}
+function testaValor(a, b) {
+  if (a !== b) {
+    resposta.innerHTML = 'Errou! Tente novamente';
   } else {
-      resposta.innerHTML = 'Acertou!';
-      scoreInicial += 3;
-      scoreNode.innerHTML = scoreInicial;
-   }
+    resposta.innerHTML = 'Acertou!';
+    scoreInicial += 3;
+    scoreNode.innerHTML = scoreInicial;
   }
+}
 
-  sortColor();
-  colorCorrect();
- 
-  colorOne.addEventListener('click', function () {
-    apagaClass();
-    colorOne.className = 'ball selected';
+sortColor();
+colorCorrect();
 
-    let textoSelecionado = colorOne.style.backgroundColor.toString().replace('rgb', '');
-    testaValor(textoSelecionado, textoRGB.innerHTML);
+colorOne.addEventListener('click', function () {
+  apagaClass();
+  colorOne.className = 'ball selected';
 
-  });
+  const textoSelecionado = colorOne.style.backgroundColor.toString().replace('rgb', '');
+  testaValor(textoSelecionado, textoRGB.innerHTML);
+});
 
-  colorTwo.addEventListener('click', function () {
-    apagaClass();
-    colorTwo.className = 'ball selected';
+colorTwo.addEventListener('click', function () {
+  apagaClass();
+  colorTwo.className = 'ball selected';
 
-    let textoSelecionado = colorTwo.style.backgroundColor.toString().replace('rgb', '');
-    testaValor(textoSelecionado, textoRGB.innerHTML);
-  });
+  const textoSelecionado = colorTwo.style.backgroundColor.toString().replace('rgb', '');
+  testaValor(textoSelecionado, textoRGB.innerHTML);
+});
 
-  colorThree.addEventListener('click', function () {
-    apagaClass();
-    colorThree.className = 'ball selected';
+colorThree.addEventListener('click', function () {
+  apagaClass();
+  colorThree.className = 'ball selected';
 
-    let textoSelecionado = colorThree.style.backgroundColor.toString().replace('rgb', '');
-    testaValor(textoSelecionado, textoRGB.innerHTML);
-  });
+  const textoSelecionado = colorThree.style.backgroundColor.toString().replace('rgb', '');
+  testaValor(textoSelecionado, textoRGB.innerHTML);
+});
 
-  colorFour.addEventListener('click', function () {
-    apagaClass();
-    colorFour.className = 'ball selected';
+colorFour.addEventListener('click', function () {
+  apagaClass();
+  colorFour.className = 'ball selected';
 
-    let textoSelecionado = colorFour.style.backgroundColor.toString().replace('rgb', '');
-    testaValor(textoSelecionado, textoRGB.innerHTML);
-  });
+  const textoSelecionado = colorFour.style.backgroundColor.toString().replace('rgb', '');
+  testaValor(textoSelecionado, textoRGB.innerHTML);
+});
 
-  colorFive.addEventListener('click', function () {
-    apagaClass();
-    colorFive.className = 'ball selected';
+colorFive.addEventListener('click', function () {
+  apagaClass();
+  colorFive.className = 'ball selected';
 
-    let textoSelecionado = colorFive.style.backgroundColor.toString().replace('rgb', '');
-    testaValor(textoSelecionado, textoRGB.innerHTML);
-  });
+  const textoSelecionado = colorFive.style.backgroundColor.toString().replace('rgb', '');
+  testaValor(textoSelecionado, textoRGB.innerHTML);
+});
 
-  colorSix.addEventListener('click', function () {
-    apagaClass();
-    colorSix.className = 'ball selected';
+colorSix.addEventListener('click', function () {
+  apagaClass();
+  colorSix.className = 'ball selected';
 
-    let textoSelecionado = colorSix.style.backgroundColor.toString().replace('rgb', '');
-    testaValor(textoSelecionado, textoRGB.innerHTML);
-  });
+  const textoSelecionado = colorSix.style.backgroundColor.toString().replace('rgb', '');
+  testaValor(textoSelecionado, textoRGB.innerHTML);
+});
 
-btnReinicia.addEventListener('click', function(){
+btnReinicia.addEventListener('click', function () {
   sortColor();
   apagaClass();
-  colorCorrect()
+  colorCorrect();
   resposta.innerHTML = 'Escolha uma cor';
-})
+});
