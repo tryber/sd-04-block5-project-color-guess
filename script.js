bdea27922a088ecc956132bd4f3aeda388330a19
deboracosilveira <deboracosilveira@gmail.com>
2020-04-19 23:33:02 -0300

    requirement-5/6

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const rgbColor = document.getElementById('rgb-color');
 const ballsContainer = document.getElementById('ballsContainer');
 const balls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
+const reset = document.getElementById('reset-game');
 
 // Generating a random number multiple of 255 
 function random() {
@@ -47,7 +48,13 @@ function ballsBgColor() {
     }
   }
 }
-
+// Restart button
+reset.addEventListener('click', function () {
+  answer.innerText = "";
+  generateRgb();
+  ballsBgColor();
+})
+// To do when load window
 window.onload = function () {
   generateRgb();
   ballsBgColor();
