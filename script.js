20419c9b57630b0a63e37bb4984b783915bf3066
deboracosilveira <deboracosilveira@gmail.com>
2020-04-20 14:42:23 -0300

    Bonus requirement

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const rgbColor = document.getElementById('rgb-color');
 const balls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
 const reset = document.getElementById('reset-game');
+const score = document.getElementById('score');
+let points = 0;
 
 // Generating a random number multiple of 255
 function random() {
@@ -24,6 +26,8 @@ for (let i = 0; i < balls.length; i += 1) {
 
     if (userAnswer === correctAnswer) {
       answer.innerText = 'Acertou!';
+      points += 3;
+      score.innerHTML = points;
     } else {
       answer.innerText = 'Errou! Tente novamente';
     }
