97cee5ccbb180467208867cc9d470cdfc04d77ec
igorgbr <igorgphotoarte@gmail.com>
2020-04-19 22:31:27 -0300

    logica pronta(?)

--- script.js
+++ script.js
@@ -7,29 +7,20 @@ const colorSix = document.querySelector('#color-six');
 
 const textoRGB = document.querySelector('#rgb-color');
 const colorPainel = document.querySelector('#colors');
+const resposta = document.querySelector('#answer');
+const ball = document.querySelectorAll('.ball');
+const btnReinicia = document.querySelector('#reset-game');
 
 function sortColor() {
-    // const corAleatoria1 = arrayCores[Math.floor(Math.random() * arrayCores.length)];
-    // const randoColorTwo = arrayCores[Math.floor(Math.random() * arrayCores.length)];
-    // const randoColorGreen = arrayCores[Math.floor(Math.random() * arrayCores.length)];
-
   colorOne.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
   colorTwo.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
   colorThree.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
   colorFour.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
   colorFive.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
   colorSix.style.backgroundColor = `rgb(${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)},${Number.parseInt(Math.random() * 255, 10)})`;
-
 }
-
-  sortColor();
-
-  let texto = ((colorThree.style.backgroundColor).toString()).replace('rgb', '');
-
-  textoRGB.innerHTML = texto;
-  
-
-  function apagaClass() {
+ 
+function apagaClass() {
     colorOne.className = 'ball';
     colorTwo.className = 'ball';
     colorThree.className = 'ball';
@@ -38,32 +29,76 @@ function sortColor() {
     colorSix.className = 'ball';
   }
 
+  function colorCorrect(){
+    for(i = 0; i < ball.length; i += 1){
+    let texto = ((ball[(Number.parseInt(Math.random() * (6 - 1) + 1))].style.backgroundColor).toString()).replace('rgb', '');
+      textoRGB.innerHTML = texto;
+      
+    }
+
+  }
+  function testaValor(a, b){
+    if(a != b) {
+      resposta.innerHTML = 'Errou! Tente novamente';
+  } else {
+      resposta.innerHTML = 'Resposta correta!';
+   }
+  }
+
+  sortColor();
+  colorCorrect();
+ 
   colorOne.addEventListener('click', function () {
     apagaClass();
     colorOne.className = 'ball selected';
+
+    let textoSelecionado = colorOne.style.backgroundColor.toString().replace('rgb', '');
+    testaValor(textoSelecionado, textoRGB.innerHTML);
   });
 
   colorTwo.addEventListener('click', function () {
     apagaClass();
     colorTwo.className = 'ball selected';
+
+    let textoSelecionado = colorTwo.style.backgroundColor.toString().replace('rgb', '');
+    testaValor(textoSelecionado, textoRGB.innerHTML);
   });
 
   colorThree.addEventListener('click', function () {
     apagaClass();
     colorThree.className = 'ball selected';
+
+    let textoSelecionado = colorThree.style.backgroundColor.toString().replace('rgb', '');
+    testaValor(textoSelecionado, textoRGB.innerHTML);
   });
 
   colorFour.addEventListener('click', function () {
     apagaClass();
     colorFour.className = 'ball selected';
+
+    let textoSelecionado = colorFour.style.backgroundColor.toString().replace('rgb', '');
+    testaValor(textoSelecionado, textoRGB.innerHTML);
   });
 
   colorFive.addEventListener('click', function () {
     apagaClass();
     colorFive.className = 'ball selected';
+
+    let textoSelecionado = colorFive.style.backgroundColor.toString().replace('rgb', '');
+    testaValor(textoSelecionado, textoRGB.innerHTML);
   });
 
   colorSix.addEventListener('click', function () {
     apagaClass();
     colorSix.className = 'ball selected';
+
+    let textoSelecionado = colorSix.style.backgroundColor.toString().replace('rgb', '');
+    testaValor(textoSelecionado, textoRGB.innerHTML);
   });
+
+btnReinicia.addEventListener('click', function(){
+  sortColor();
+  apagaClass();
+  colorCorrect()
+  resposta.innerHTML = 'Escolha uma cor';
+})
