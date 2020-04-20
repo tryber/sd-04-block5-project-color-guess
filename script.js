b0fb48ddd7462d81e63ed22762e57e133759f21e
deboracosilveira <deboracosilveira@gmail.com>
2020-04-19 23:51:56 -0300

    requirement-4 again

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 // Variables
-const title = document.getElementById('title');
 const rgbColor = document.getElementById('rgb-color');
-const ballsContainer = document.getElementById('ballsContainer');
 const balls = document.querySelectorAll('.ball');
 const answer = document.getElementById('answer');
 const reset = document.getElementById('reset-game');
 
-// Generating a random number multiple of 255 
+// Generating a random number multiple of 255
 function random() {
-  const random = Math.round(Math.random() * 255);
-  return random;
+  const randomNumber = Math.round(Math.random() * 255);
+  return randomNumber;
 }
 // Generating random rgb
 function generateRgb() {
@@ -26,22 +24,19 @@ for (let i = 0; i < balls.length; i += 1) {
 
     if (userAnswer === correctAnswer) {
       answer.innerText = 'Acertou!';
-      console.log("correctAnswer");
     } else {
       answer.innerText = 'Errou! Tente novamente';
-      console.log("wrongAnswer");
     }
   });
 }
 // Generating balls background color
 function ballsBgColor() {
-  let randomNumber = Math.floor(Math.random() * balls.length)// Random number between 0 and 5
+  const randomNumber = Math.floor(Math.random() * balls.length);// Random number between 0 and 5
   for (let i = 0; i < balls.length; i += 1) {
-    if (i === randomNumber) {// To put the answer in a random ball
+    if (i === randomNumber) { // To put the answer in a random ball
       let correctAnswer = rgbColor.innerText;
       correctAnswer = 'rgb' + correctAnswer;
       balls[i].style.backgroundColor = correctAnswer;
-      console.log(randomNumber)
     } else {
       const rgb = 'rgb(' + random() + ', ' + random() + ', ' + random() + ')';
       balls[i].style.backgroundColor = rgb;
@@ -50,12 +45,12 @@ function ballsBgColor() {
 }
 // Restart button
 reset.addEventListener('click', function () {
-  answer.innerText = "";
+  answer.innerText = 'Escolha uma cor';
   generateRgb();
   ballsBgColor();
-})
+});
 // To do when load window
 window.onload = function () {
   generateRgb();
   ballsBgColor();
-}
+};
