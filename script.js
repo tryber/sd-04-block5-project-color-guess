94709b07ae53f4c9296b9496352f369eb6656d60
danilossousa <danilosantossousa@gmail.com>
2020-04-21 09:01:01 -0300

    v0.4.0.1

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+let i;
 const butReset = document.getElementById('reset-game');
 const elRgbColor = document.getElementById('rgb-color');
+const balls = document.getElementsByClassName('ball');
+const elAnswer = document.getElementById('answer');
 
-function randomRGB() {
+function randomRgb() {
   const randomNumber1 = Math.floor(Math.random() * 257);
   const randomNumber2 = Math.floor(Math.random() * 257);
   const randomNumber3 = Math.floor(Math.random() * 257);
 
-  elRgbColor.innerText = `(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
+  return `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
 }
 
-butReset.addEventListener('click', randomRGB);
+function randomRgbBall() {
+  const randomIndex = Math.floor(Math.random() * 6);
 
-window.onload = function () {
-  randomRGB();
-};
+  balls[randomIndex].style.backgroundColor = `rgb${elRgbColor.innerText}`;
+}
+
+function randomRgbBalls() {
+  for (i = 0; i < balls.length; i += 1) {
+    if (balls[i].style.backgroundColor !== `rgb${elRgbColor.innerText}`) {
+      balls[i].style.backgroundColor = randomRgb();
+    }
+  }
+}
+
+butReset.addEventListener('click', function () {
+  document.location.reload(true);
+});
+
+for (i = 0; i < balls.length; i += 1) {
+  balls[i].addEventListener('click', function (e) {
+    if (e.target.style.backgroundColor.slice(3) === elRgbColor.innerText) {
+      elAnswer.innerText = 'Acertou!';
+    } else {
+      elAnswer.innerText = 'Errou! Tente novamente!';
+    }
+  });
+}
+
+elRgbColor.innerText = randomRgb().slice(3);
+
+randomRgbBall();
+randomRgbBalls();
